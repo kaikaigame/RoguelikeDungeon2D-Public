d8e0f018594ae4b3dc4559319ba31eee6299b4ee
kaikaigame <kairemake@outlook.com>
2021-06-23 03:20:04 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    public float speed;
+    Vector2 movement;
+
+    public RawImage rawImage;
+    bool isOpen;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isOpen = !isOpen;
+            rawImage.gameObject.SetActive(isOpen);
+        }
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x != 0)
+        {
+            transform.localScale = new Vector3(movement.x, 1, 1);
+        }
+
+        SwitchAnim();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void SwitchAnim()
+    {
+        anim.SetFloat("speed", movement.magnitude);
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Roguelike Dungeon 2D
+  
+Just the practice.  
+Not available for commercial use.  
+  
+My special thanks go to M-Studio  
+https://m-studio-m.github.io  
+  
+Free Assets downloaded from  
+  
+https://unity.cn/projects/chuang-jian-roguelikesui-ji-di-xia-cheng-yi  
+  
+https://unity.cn/projects/chuang-jian-roguelikesui-ji-di-xia-cheng-er  
+  
+https://unity.cn/projects/chuang-jian-roguelikesui-ji-di-xia-cheng-san  
+  
+https://elthen.itch.io/pixel-art-adventurer-sprites  
+  
+https://elthen.itch.io/2d-pixel-art-dungeon-tileset  
+  
+https://elthen.itch.io/  
+  
+Unity m_EditorVersion:  2021.1.12f1  
+  
+KEYBOARD  
+  
+UP: w or up  
+DOWN: s or down  
+LEFT: a or left  
+RIGHT: d or right  
+MAP: m  
+  
